131efeda3c4484ae92039aa8b89983eb45f611f9
youger <liyou0203@gmail.com>
2023-04-21 20:12:16 +0800

    add more resolution for find duplicate in array

--- algorithms/golang/array/find_duplicates.go
+++ algorithms/golang/array/find_duplicates.go
@@ -1,6 +1,6 @@
 package array
 
-func findDuplicate(nums []int) (res []int) {
+func findDuplicates(nums []int) (res []int) {
 	res = make([]int, 0)
 	count := len(nums)
 	flag := make(map[int]int, 0)
@@ -10,6 +10,36 @@ func findDuplicate(nums []int) (res []int) {
 	for k, v := range flag {
 		if v == 2 {
 			res = append(res, k)
+		}
+	}
+	return
+}
+
+func findDuplicates2(nums []int) (res []int) {
+	res = make([]int, 0)
+	for _, v := range nums {
+		if v < 0 {
+			v = -v
+		}
+		index := v - 1
+		if nums[index] > 0 {
+			nums[index] = -nums[index]
+		} else {
+			res = append(res, v)
+		}
+	}
+	return
+}
+
+func findDuplicates3(nums []int) (ans []int) {
+	for i := range nums {
+		for nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+		}
+	}
+	for i, num := range nums {
+		if num-1 != i {
+			ans = append(ans, num)
 		}
 	}
 	return
